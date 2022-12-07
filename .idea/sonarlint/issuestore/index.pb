b578db7ba9487ee89f2f0b323826417666eef010
nytex3d <tarisjan@gmail.com>
2022-12-07 20:49:39 +0100

    added context for number in line 65

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
->
-files/test.txt,a\d\ad0f5d3066f71b3808ef8daa595392e2787c7806
--- aoc_2.py
+++ aoc_2.py
@@ -16,11 +16,11 @@ def get_points(a: str, b: str):
     return temp_score
 
 
-src = get_input_from_file("files/AOC_1/input.txt")
+src = get_input_from_file("files/AOC_1/test.txt")
 sets = []
 score = 0
-for line in src:
-    sets.append(line.split(" "))
+for element in src:
+    sets.append(element.split(" "))
 
 points = {
     "A": 1,
@@ -31,23 +31,23 @@ points = {
     "Z": 3,
 }
 
-order = [3,1,2,3,1,2]
+order = [3, 1, 2, 3, 1, 2]
 
-#part 1
+# part 1
 for duel in sets:
     score += get_points(duel[0], duel[1])
 
 print(f"part 1:{score}")
 
-#part 2
+# part 2
 score = 0
 for duel in sets:
     if duel[1] == "X":
-        score += 0 + order[order.index(points[duel[0]])-1]
+        score += 0 + order[order.index(points[duel[0]]) - 1]
     elif duel[1] == "Y":
         score += 3 + points[duel[0]]
 
     else:
-        score += 6 + order[order.index(points[duel[0]])+1]
+        score += 6 + order[order.index(points[duel[0]]) + 1]
 
 print(f"part 2:{score}")
